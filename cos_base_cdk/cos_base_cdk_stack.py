a4e27e321cef94fb866dce4bd9d468d909960095
dark-cos <darkadrenal1ne@hotmail.com>
2023-10-08 20:20:10 -0400

    lambda function added

--- cos_base_cdk/cos_base_cdk_stack.py
+++ cos_base_cdk/cos_base_cdk_stack.py
@@ -1,7 +1,8 @@
 from aws_cdk import (
-    # Duration,
+    Duration,
     Stack,
     # aws_sqs as sqs,
+    aws_lambda as lambda_
 )
 from constructs import Construct
 
@@ -11,6 +12,15 @@ class CosBaseCdkStack(Stack):
         super().__init__(scope, construct_id, **kwargs)
 
         # The code that defines your stack goes here
+
+        lambda_function = lambda_.Function(
+            self,
+            "generic_function",
+            code=lambda_.Code.from_asset('functions/generic_lambda'),
+            handler="index.lambda_handler",
+            timeout=Duration.seconds(10),
+            runtime=lambda_.Runtime.PYTHON_3_9
+        )
 
         # example resource
         # queue = sqs.Queue(
---
+++ functions/generic_lambda/index.py
@@ -0,0 +1,5 @@
+
+def lambda_handler(event, context):
+    print("Hello World!")
+
+    return
